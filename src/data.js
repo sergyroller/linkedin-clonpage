b9335a9e126f7f726dc260d4476a43e5a5c69663
Sergio Ruiz <sergio.ruiz.teleco@gmail.com>
2022-04-13 08:58:36 +0200

    card dynamic

--- src/data.js
+++ src/data.js
@@ -2,23 +2,23 @@ const arrayPost = [
   {
     type: {
       2: {
-        name: "Correos",
-        followers: 46.233
+        name: "Card dinámica",
+        followers: 12.233
       }
     },
-    photoPerfil: "https://media-exp1.licdn.com/dms/image/C4D0BAQEZTZJbr9AiTQ/company-logo_100_100/0/1597217645738?e=2147483647&v=beta&t=Gh65wEtKDnsgH8IbRtWq0groYnu5dwnryJPWPktQZqI",
-    time: "1 semana",
-    text: "Correos Cargo, nuestro nuevo servicio de transporte aéreo de paquetería y mercancías da respuesta al aumento de envíos motivado por el auge del comercio electrónico, además de favorecer la internacionalización de las empresas convirtiéndose en referente del eje logístico América-Europa-Asia.",
+    photoPerfil: "https://media-exp1.licdn.com/dms/image/C4D0BAQGzzRPO_4NHRA/company-logo_100_100/0/1617698580953?e=2147483647&v=beta&t=zXQjju0QC9UroeEsoc27Qjb4nuUmeRDZFSZd4gRlOZY",
+    time: "1 mes",
+    text: "Esta card se está mostrando obteniendo los datos de un array de objetos llamado data.js",
     publicationType: {
       1: "image",
       2: "video",
     },
-    mediaPublication: "https://media-exp1.licdn.com/dms/image/sync/C4D27AQFghhsCQDTvSw/articleshare-shrink_1280_800/0/1649357016705?e=1649757600&v=beta&t=FayegpvHB8jUXDBwLSDADiEvwIJc-ANzjioclrt85og",
-    textImage: "Inclusión financiera, clave para la vertebración del territorio",
-    moreInfoUrl: "cincodias.elpais.com",
-    moreInfoTime: "3 min de lectura",
+    mediaPublication: "https://media-exp1.licdn.com/dms/image/sync/D4D18AQGRl9OWEp6kpg/companyUpdate-article-image-shrink_627_1200/0/1648541061793/ciberseguridadjpg?e=2147483647&v=beta&t=EBhgtV-m4dyL8-TfgqToQQQ1dmPeyhfWeSM9Ch0O0uw",
+    textImage: "Solicitar Subvención para CIBERSEGURIDAD | Kit Digital - Datacom.Global",
+    moreInfoUrl: "datacom.global",
+    moreInfoTime: "10 min de lectura",
     interactions: {
-      likes: ["Lucia Ruiz Martinez", "Sergio Ruiz"],
+      likes: ["Sergio Ruiz", "otra persona", "otra persona", "otra persona", "otra persona", "otra persona", "otra persona"],
       recommeded: ["Sergio Ruiz"],
       shares: ["Alfredo Gutierrez"]
     }
--- src/index.js
+++ src/index.js
@@ -235,7 +235,119 @@ function dynamicPublication() {
   followersP.textContent = arrayPost[0].type[2].followers + " seguidores";
   infoActor.insertAdjacentElement("beforeend", followersP);
 
+  const dateTimeInfoActor = document.createElement("div");
+  dateTimeInfoActor.classList.add("datetime");
+  infoActor.appendChild(dateTimeInfoActor);
+
+  const dateTimeInfoActorP = document.createElement("p");
+  dateTimeInfoActorP.textContent = arrayPost[0].time + " • ";
+  dateTimeInfoActor.appendChild(dateTimeInfoActorP);
+
+  const dateTimeInfoActorSvg = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16" fill="currentColor" class="mercado-match" width="16" height="16" focusable="false">
+      <path d="M8 1a7 7 0 107 7 7 7 0 00-7-7zM3 8a5 5 0 011-3l.55.55A1.5 1.5 0 015 6.62v1.07a.75.75 0 00.22.53l.56.56a.75.75 0 00.53.22H7v.69a.75.75 0 00.22.53l.56.56a.75.75 0 01.22.53V13a5 5 0 01-5-5zm6.24 4.83l2-2.46a.75.75 0 00.09-.8l-.58-1.16A.76.76 0 0010 8H7v-.19a.51.51 0 01.28-.45l.38-.19a.74.74 0 01.68 0L9 7.5l.38-.7a1 1 0 00.12-.48v-.85a.78.78 0 01.21-.53l1.07-1.09a5 5 0 01-1.54 9z"></path>
+    </svg>
+    `;
+  dateTimeInfoActor.insertAdjacentHTML("beforeend", dateTimeInfoActorSvg);
+
   const headerFeedATagTwo = document.createElement("a");
   headerFeedATagTwo.setAttribute("href", "#");
   headerFeed.appendChild(headerFeedATagTwo);
+
+  const followDiv = document.createElement("div");
+  followDiv.classList.add("follow", "no-mobile");
+  headerFeedATagTwo.appendChild(followDiv);
+
+  // BEGIN PRACTICES WITH TEMPLATE STRINGS
+  const svgFollowP = `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16" fill="currentColor" class="mercado-match" width="16" height="16" focusable="false">
+        <path d="M14 9H9v5H7V9H2V7h5V2h2v5h5z"></path>
+      </svg>
+      <p>Seguir</p>
+  `;
+  followDiv.insertAdjacentHTML("beforeend", svgFollowP);
+
+  const innerTextFeed = `
+  <p class="text">
+  ${arrayPost[0].text}
+  </p>`;
+  textFeed.insertAdjacentHTML("beforeend", innerTextFeed);
+
+  const innerImageFeed = `
+  <img src="${arrayPost[0].mediaPublication}">
+  <a href="#">
+    <div class="image-details">
+      <p>${arrayPost[0].textImage}</p>
+      <div class="more-details">
+        <p>${arrayPost[0].moreInfoUrl}</p>
+        <p>•</p>
+        <p>${arrayPost[0].moreInfoTime}</p>
+      </div>
+    </div>
+  </a>
+  `;
+  imageFeed.insertAdjacentHTML("beforeend", innerImageFeed);
+
+  // CHECK IF PUBLICATION HAVE INTERACTIONS
+  for (const values in arrayPost[0].interactions) {
+    if (arrayPost[0].interactions[values].length > 0) {
+      const likesDiv = document.createElement("div");
+      likesDiv.classList.add("likes");
+      likesWrapper.appendChild(likesDiv);
+      break;
+    }
+  }
+
+  for (const values in arrayPost[0].interactions) {
+    const likesDiv = document.querySelector(".likes-wrapper .likes");
+    const imgLikes = document.createElement("img");
+    const p = document.createElement("p");
+    p.setAttribute("id", "people");
+
+    if (arrayPost[0].interactions[values].length > 0) {
+      switch (values) {
+        case "likes":
+          console.log(arrayPost[0].interactions[values]);
+          imgLikes.setAttribute("src", "https://static-exp1.licdn.com/sc/h/8ekq8gho1ruaf8i7f86vd1ftt");
+          if (arrayPost[0].interactions[values].length < 2) {
+            p.textContent = arrayPost[0].interactions[values][0];
+          } else if (arrayPost[0].interactions[values].length === 2) {
+            p.textContent = arrayPost[0].interactions[values][0] + " y " + (arrayPost[0].interactions[values].length - 1) + " persona más";
+          } else {
+            p.textContent = arrayPost[0].interactions[values][0] + " y " + (arrayPost[0].interactions[values].length - 1) + " personas más";
+          }
+          likesDiv.appendChild(p);
+          p.insertAdjacentElement("beforebegin", imgLikes);
+          break;
+      }
+    }
+  }
+  const innerButtonInteractions = `
+  <a href="#" class="button">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+     <path d="M19.46 11l-3.91-3.91a7 7 0 01-1.69-2.74l-.49-1.47A2.76 2.76 0 0010.76 1 2.75 2.75 0 008 3.74v1.12a9.19 9.19 0 00.46 2.85L8.89 9H4.12A2.12 2.12 0 002 11.12a2.16 2.16 0 00.92 1.76A2.11 2.11 0 002 14.62a2.14 2.14 0 001.28 2 2 2 0 00-.28 1 2.12 2.12 0 002 2.12v.14A2.12 2.12 0 007.12 22h7.49a8.08 8.08 0 003.58-.84l.31-.16H21V11zM19 19h-1l-.73.37a6.14 6.14 0 01-2.69.63H7.72a1 1 0 01-1-.72l-.25-.87-.85-.41A1 1 0 015 17l.17-1-.76-.74A1 1 0 014.27 14l.66-1.09-.73-1.1a.49.49 0 01.08-.7.48.48 0 01.34-.11h7.05l-1.31-3.92A7 7 0 0110 4.86V3.75a.77.77 0 01.75-.75.75.75 0 01.71.51L12 5a9 9 0 002.13 3.5l4.5 4.5H19z"></path>
+    </svg>
+    <p>Recomendar</p>
+  </a>
+  <a href="#" class="button">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+      <path d="M7 9h10v1H7zm0 4h7v-1H7zm16-2a6.78 6.78 0 01-2.84 5.61L12 22v-4H8A7 7 0 018 4h8a7 7 0 017 7zm-2 0a5 5 0 00-5-5H8a5 5 0 000 10h6v2.28L19 15a4.79 4.79 0 002-4z"></path>
+    </svg>
+    <p>Comentar</p>
+  </a>
+  <a href="#" class="button">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+      <path d="M23 12l-4.61 7H16l4-6H8a3.92 3.92 0 00-4 3.84V17a4 4 0 00.19 1.24L5.12 21H3l-.73-2.22A6.4 6.4 0 012 16.94 6 6 0 018 11h12l-4-6h2.39z"></path>
+    </svg>
+    <p>Compartir</p>
+  </a>
+  <a href="#" class="button no-mobile">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+      <path d="M21 3L0 10l7.66 4.26L16 8l-6.26 8.34L14 24l7-21z"></path>
+    </svg>
+    <p>Enviar</p>
+  </a>
+`;
+
+  buttonsInteraction.insertAdjacentHTML("beforeend", innerButtonInteractions);
 }
